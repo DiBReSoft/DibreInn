4b42e8f4be529278b13b38d13d09c527c7d43b20
luciano.lourenco <luciano.lourenco@CAS419440C13631.senacsp.edu.br>
2015-05-08 22:33:27 -0300

    Criada a classe modelo "Endereço" para receber os dados de endereço fora da classe pessoa;
    Foi retirado os atributos referentes ao endereços, e passados para a classe endereço

---
+++ src/java/br/com/lebrehotel/dibreinn/model/pessoa/Endereco.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lebrehotel.dibreinn.model.pessoa;
+
+/**
+ *
+ * @author luciano.lourenco
+ */
+public class Endereco {
+    
+    private String logradouro;
+    private String numero;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private String Complemento;
+    private String estado;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String Complemento) {
+        this.Complemento = Complemento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
